d9b98cc17b9f4cfc3751f48ae8ba9bef60cbdc59
Wiktor <wiktor.otrebus01@gmail.com>
2024-07-01 13:02:10 +0200

    Add logs functionality to skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,12 @@
 if [ "$1" == "--date" ]; then
   date
 fi
+if [ "$1" == "--logs" ]; then
+  num=100
+  if [ -n "$2" ]; then
+    num=$2
+  fi
+  for i in $(seq 1 $num); do
+    echo "Log $i, created by $0 on $(date)" > log_$i.txt
+  done
+fi
