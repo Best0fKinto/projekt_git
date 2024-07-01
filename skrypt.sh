4ee3469d7670ae8b1907423edff622111dc1a1b8
Wiktor <wiktor.otrebus01@gmail.com>
2024-07-01 13:03:09 +0200

    Add single-dash flag support to skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -17,3 +17,21 @@ if [ "$1" == "--help" ]; then
   echo "--logs [number]: Creates [number] log files"
   echo "--help: Displays this help message"
 fi
+if [ "$1" == "-d" ]; then
+  date
+fi
+if [ "$1" == "-l" ]; then
+  num=100
+  if [ -n "$2" ]; then
+    num=$2
+  fi
+  for i in $(seq 1 $num); do
+    echo "Log $i, created by $0 on $(date)" > log_$i.txt
+  done
+fi
+if [ "$1" == "-h" ]; then
+  echo "Available options:"
+  echo "--date, -d: Displays the current date"
+  echo "--logs [number], -l [number]: Creates [number] log files"
+  echo "--help, -h: Displays this help message"
+fi
